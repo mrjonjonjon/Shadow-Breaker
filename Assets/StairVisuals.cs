7922b8ce190f2107e591b134c70e049241d4f406
jakaba <kakwekuakaba@gmail.com>
2023-10-27 10:39:56 -0700

    stairs mostly working

--- Assets/StairVisuals.cs
+++ Assets/StairVisuals.cs
@@ -27,7 +27,10 @@ Material _stairsMat;
         _stairsMat = GetComponent<Stairs>()._stairsMat;
         //visuals and size synch
 _collider2D.size = new Vector2(width,depth);
+transform.Find("sprite/stairs").localScale = new Vector3(width,depth,1);
+transform.Find("sprite").transform.localPosition = new Vector3(0,z_bot,-z_bot);
 _stairsMat.SetFloat("_Height",z_top-z_bot);
+//_stairsMat.SetFloat("_Depth",depth);
 //float angle = 360f*Mathf.Atan2(z_top-z_bot,width)/(3.1415926535f*2f);
 //transform.Find("sprite/stairs").transform.rotation = Quaternion.Euler(0,-angle,angle);//deends on slope
 
