330227e177445ec165365b05dc75c43411959c14
jakaba <kakwekuakaba@gmail.com>
2023-11-07 19:45:47 -0800

    fixed executeineditor stuff

--- Assets/EntityPropertyBlock.cs
+++ Assets/EntityPropertyBlock.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-//[ExecuteInEditMode]
+[ExecuteInEditMode]
 
 public class EntityPropertyBlock : MonoBehaviour
 
@@ -43,16 +43,16 @@ public class EntityPropertyBlock : MonoBehaviour
     private void SetFloat()
     {
         //rend=bottom, rend2=top
-        if(MovementController.instance==null){
-            return;
-        }
-        Physics playerPhysics= MovementController.instance.transform.GetComponent<Physics>();
+        //if(MovementController.instance==null){
+        //    return;
+        //}
+        //Physics playerPhysics= MovementController.instance.transform.GetComponent<Physics>();
 
         rend.GetPropertyBlock(_propertyBlock); 
         // Get previously set values. They will reset otherwise
    
-        Vector4 playerPos = new Vector4(playerPhysics.transform.position.x,playerPhysics.transform.position.y,playerPhysics.zpos,0f);
-        Vector4 pos = new Vector4(transform.position.x,transform.position.y,GetComponent<Physics>().zpos,0f);
+        //Vector4 playerPos = new Vector4(playerPhysics.transform.position.x,playerPhysics.transform.position.y,playerPhysics.zpos,0f);
+       // Vector4 pos = new Vector4(transform.position.x,transform.position.y,GetComponent<Physics>().zpos,0f);
         _propertyBlock.SetFloat("_Height",GetComponent<Physics>().height);
         _propertyBlock.SetFloat("_Flat",0f);
          _propertyBlock.SetFloat("_Bottom",transform.Find("sprite/bottom").position.y - GetComponent<Physics>().height/2f);
